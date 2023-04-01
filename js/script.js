24d971aa1d521e13f642a2ab6a7624f74db8f7af
Andrestipitech <119084933+Andrestipitech@users.noreply.github.com>
2023-04-01 16:23:36 -0500

    calculadora estilos

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 
 
 function sumar(){
-	let num1 = document.getElementById('num1').value;
-	let num2 = document.getElementById('num2').value;
-	let suma = parseInt(num1) + parseInt(num2);
+	let num1 = document.getElementById('num1');
+	let num2 = document.getElementById('num2');
+	let suma = parseInt(num1.value) + parseInt(num2.value);
 	console.log("El resultado de la suma es: " + suma);
+	let ubicacionR = document.getElementById('resultado_suma');
+	ubicacionR.innerHTML=`<p class='resul'> ${suma} </p> `;
+
+	num1.value ="";
+	num2.value ="";
+
 }
